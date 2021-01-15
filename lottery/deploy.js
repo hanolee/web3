b4be859f536dc3a7df53ca6dc70573414066d6a4
hanolee <hano.dev.kr@gmail.com>
2021-01-15 16:47:54 +0900

    deploy, compile js

--- lottery/deploy.js
+++ lottery/deploy.js
@@ -1,3 +1,23 @@
-const HDWalletProvider = require('truffle')
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface, bytecode} = require('./compile');
 
 
+const provider = new HDWalletProvider(
+    'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
+    "link to infura"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data : bytecode, arguments: ['Hi there!']})
+        .send({gas : '1000000', from : accounts[0]});
+
+    console.log('Contract deployed to', result.options.address);
+    
+}
--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -2,20 +2,42 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile')
 
-class Car {
-    park(){
-        return 'stopped';
-    }
-    drive(){
-        return 'vroom';
-    }
-}
+let accounts
 
-describe('Car', ()=>{
-    it('can park', ()=>{
-        const car = new Car();
-        assert.strictequal(car.park(), 'stopped')
+beforeEach(() => {
+//Get a list of all accounts
+
+    // web3.eth.getAccounts()
+    //     .then(fetchedAccounts => {
+    //         console.log(fetchedAccounts);
+    //     });
+
+    beforeEach(async() => {
+        //get a list of all accounts
+        accounts = await web3.eth.getAccounts();
+        //Use One of those accounts to deploy
+        //the contract
+        lottery = await new Web3.eth.Contract(JSON.parse(interface))
+            .deploy({data : bytecode, arguments : ['Hi there!']})
+            .send({from : accounts[0], gas : '1000000'});
     })
+});
+
+describe('Lottery', () => {
+    it('deploys a contract', () => {
+        console.log(lottery);
+        assert.ok(lottery.options.address);
+    });
+    it('has a default message', async () => {
+        const message = await lottery.methods.message().call();
+        assert.strictEqual(message, 'Hi there!');
+    });
+    it('can change the message', async () => {
+        await lottery.methods.setMessage('bye').send({from : accounts[0]})
+        const message = await lottery.methods.message().call()
+        assert.strictEqual(message, 'bye')
+    });
 })
 
